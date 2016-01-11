eec1c4c61733a5e21d820e01baca8d4ed11a12d1
Lopaka Delp <lopaka@rightscale.com>
2016-01-10 21:28:43 -0800

    Code in lockfile. Redirect STDOUT and STDERR to logfile. Verify s3 bucket location string.

--- ec2/backup_s3.sh
+++ ec2/backup_s3.sh
@@ -1,10 +1,36 @@
 #!/bin/bash -e
 
+# redirect STDOUT and STDERR to logfile
+logfile=/tmp/s3backup-`date +%Y%m%d-%H%M%S`.log
+exec > $logfile 2>&1
+
+# check for lock file and if backup already running
+lockfile=/var/run/s3_backup.lock
+if [ -e ${lockfile} ]; then
+  echo "Lock file exists - ${lockfile} - verifying pid"
+  old_pid=`cat ${lockfile}`
+  pid_check=`ps -h -${old_pid} || echo 'fail'`
+  if [[ "${pid_check}" == 'fail' ]]; then
+    echo "Lock file does not contain active pid (${old_pid}) - continuing"
+  else
+    echo "Backup already running - $pid_check - exiting"
+    exit 1
+  fi
+fi
+echo "Creating lock file"
+echo "$$" > ${lockfile}
+
+# verify s3cmd is installed
 s3cmd --version > /dev/null 2>&1 || (echo "s3cmd not installed" && exit 1)
 
+# verify argument is an s3 location
 dest=$1
 if [[ "$dest" == "" ]]; then
   echo "ERROR: first arg should be S3 location"
+  exit 1
+fi
+if ! [[ $dest =~ ^[sS]3:// ]]; then
+  echo "ERROR: invalid destination syntax not matching s3://: $dest"
   exit 1
 fi
 
@@ -17,7 +43,7 @@ fi
 verify_dest=`s3cmd ls $dest | wc -l`
 
 if [[ $verify_dest -gt 0 ]]; then
-  echo "Dest exists"
+  echo "Sending backup to $dest"
 else
   echo "ERROR: $dest does not exist or cannot access"
   exit 1
