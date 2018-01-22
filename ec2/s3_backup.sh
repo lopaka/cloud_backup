8c3bad656b31b100a715c26f62cade49a51f0f83
Lopaka Delp <lopaka@rightscale.com>
2018-01-21 23:56:45 -0800

    Update in s3_backup.sh to use a config file and several update updates

--- ec2/s3_backup.sh
+++ ec2/s3_backup.sh
@@ -1,21 +1,58 @@
 #!/bin/bash -e
 
-# s3_backup.sh - will backup / to s3
+# s3_backup.sh - backup directory to S3 using 's3cmd sync'
 #
-# usage: s3_backup.sh s3://bucket/dir
+# usage: s3_backup.sh /path/to/config/file
+#
+# config file example (all variables required - order does not matter):
+#   ACCESS_KEY_ID=ABCDEFGHIJKLMNOP1234
+#   SECRET_ACCESS_KEY=AbCdEfGhIjKlMnOpQrStUvWxYz78934+24jsldiu
+#   BUCKET_OBJECT=s3://bucket/object/
+#   SOURCE_DIR=/
+#   REXCLUDE='^(proc|dev|tmp|media|mnt|sys|run|var\/run|var\/lock|var\/cache\/apt\/archives)/|^swapfile|^var\/lib\/php5\/sess_' \
+#   LOG_DIR=/var/log
+
+# verify s3cmd is installed
+s3cmd --version > /dev/null 2>&1 || (echo "s3cmd not installed" && exit 1)
+
+# Read in required config file
+if [[ $# -ne 1 ]]; then
+  echo "CONFIG FILE ARG REQUIRED"
+  exit 1
+fi
+if [[ ! -e $1 ]]; then
+  echo "'$1' DOES NOT EXIST"
+  exit 1
+fi
+source "$1"
+
+# Verify all required vars
+REQUIRED_VARS="ACCESS_KEY_ID \
+  SECRET_ACCESS_KEY \
+  BUCKET_OBJECT \
+  SOURCE_DIR \
+  REXCLUDE \
+  LOG_DIR"
+for check_var in ${REQUIRED_VARS}; do
+  if [[ -z ${!check_var+x} ]]; then
+    echo "$check_var does not exist - exiting"
+    exit 1
+  fi
+done
 
 # redirect STDOUT and STDERR to logfile
-logfile=/var/log/s3backup-`date +%Y%m%d-%H%M%S`.log
-exec > $logfile 2>&1
+logfile="${LOG_DIR}/s3backup-$(date +%Y%m%d-%H%M%S).log"
+exec > "$logfile" 2>&1
 
-start_time=`date +%s`
+start_time=$(date +%s)
 echo "Starting time: ${start_time}"
+
 # check for lock file and if backup already running
 lockfile=/var/run/s3_backup.lock
 if [ -e ${lockfile} ]; then
   echo "Lock file exists - ${lockfile} - verifying pid"
-  old_pid=`cat ${lockfile}`
-  pid_check=`ps -h -p ${old_pid} || echo 'fail'`
+  old_pid=$(cat ${lockfile})
+  pid_check=$(ps -h -p "${old_pid}" || echo 'fail')
   if [[ "${pid_check}" == 'fail' ]]; then
     echo "Lock file does not contain active pid (${old_pid}) - continuing"
   else
@@ -26,41 +63,42 @@ fi
 echo "Creating lock file"
 echo "$$" > ${lockfile}
 
-# verify s3cmd is installed
-s3cmd --version > /dev/null 2>&1 || (echo "s3cmd not installed" && exit 1)
-
-# verify argument is an s3 location
-dest=$1
-if [[ "$dest" == "" ]]; then
-  echo "ERROR: first arg should be S3 location"
-  exit 1
-fi
-if ! [[ $dest =~ ^[sS]3:// ]]; then
-  echo "ERROR: invalid destination syntax not matching s3://: $dest"
+# Verify BUCKET_OBJECT is an s3 location
+if ! [[ $BUCKET_OBJECT =~ ^[sS]3:// ]]; then
+  echo "ERROR: invalid destination syntax not matching s3://: $BUCKET_OBJECT"
   exit 1
 fi
 
 # Add trailing slash to dest making sure it's a directory
-if ! [[ $dest =~ \/$ ]]; then
+if ! [[ $BUCKET_OBJECT =~ /$ ]]; then
   echo "adding trailing slash"
-  dest="$dest/"
+  BUCKET_OBJECT="${BUCKET_OBJECT}/"
 fi
 
-verify_dest=`s3cmd ls $dest | wc -l`
-
-if [[ $verify_dest -gt 0 ]]; then
-  echo "Sending backup to $dest"
+# Verify existance and access to BUCKET_OBJECT
+bucket_info=$(s3cmd info --access_key="$ACCESS_KEY_ID" --secret_key="$SECRET_ACCESS_KEY" --quiet "$BUCKET_OBJECT" || echo 'fail')
+if [[ "${bucket_info}" == 'fail' ]]; then
+  echo "ERROR: $BUCKET_OBJECT does not exist or cannot access"
+  exit 1
 else
-  echo "ERROR: $dest does not exist or cannot access"
+  echo "Sending backup to $BUCKET_OBJECT"
+fi
+
+# Verify source is a directory that exists
+if [ ! -d "${SOURCE_DIR}" ]; then
+  echo "ERROR: SOURCE_DIR (${SOURCE_DIR}) IS NOT A DIRECTORY"
   exit 1
 fi
 
 # return true in the event s3cmd fails in order to continue to end of script
+# remember, STDOUT and STDERR are sent to logfile
 s3cmd sync \
+--access_key=$ACCESS_KEY_ID \
+--secret_key=$SECRET_ACCESS_KEY \
 --verbose \
---rexclude '^(proc|dev|tmp|media|mnt|sys|run|var\/run|var\/lock|var\/cache\/apt\/archives)/|^swapfile|^var\/lib\/php5\/sess_' \
---cache-file=/var/run/s3cmd_cache \
---delete-removed / $dest || true
+--rexclude $REXCLUDE \
+--cache-file=/var/cache/s3cmd_cache \
+--delete-removed $SOURCE_DIR $BUCKET_OBJECT || true
 
-total_time=$((`date +%s`-start_time))
+total_time=$(($(date +%s)-start_time))
 echo "Total time: ${total_time}"
